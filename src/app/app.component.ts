12496d39edaf6a8780a13ec0db290a1f3b3381c1
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2020-03-15 20:47:18 +0100

    icono carrito de la compra

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,25 +6,44 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'SXEListaDeCompra2';
 
-  loginValidForm: FormGroup;
+  loginValidatingForm: FormGroup;
+  signupValidatingForm: FormGroup;
 
   constructor() {
   }
 
   ngOnInit() {
-    this.loginValidForm = new FormGroup({
+    this.loginValidatingForm = new FormGroup({
       loginFormModalEmail: new FormControl('', Validators.email),
       loginFormModalPassword: new FormControl('', Validators.required)
     });
+    this.signupValidatingForm = new FormGroup({
+      signupFormModalName: new FormControl('', Validators.required),
+      signupFormModalEmail: new FormControl('', Validators.email),
+      signupFormModalPassword: new FormControl('', Validators.required),
+    });
   }
+
   get loginFormModalEmail() {
-    return this.loginValidForm.get('loginFormModalEmail');
+    return this.loginValidatingForm.get('loginFormModalEmail');
   }
 
   get loginFormModalPassword() {
-    return this.loginValidForm.get('loginFormModalPassword');
+    return this.loginValidatingForm.get('loginFormModalPassword');
+  }
+
+  get signupFormModalName() {
+    return this.signupValidatingForm.get('signupFormModalName');
+  }
+
+  get signupFormModalEmail() {
+    return this.signupValidatingForm.get('signupFormModalEmail');
+  }
+
+  get signupFormModalPassword() {
+    return this.signupValidatingForm.get('signupFormModalPassword');
   }
 }

dbb03fb46b08419830f6c7d94fcb40fc4d162633
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2020-03-16 13:59:35 +0100

    routing carrito badge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {CatalogoComponent} from './catalogo/catalogo.component';
+import {CarritoComponent} from './carrito/carrito.component';
+import {PerfilComponent} from './perfil/perfil.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'catalogo', component: CatalogoComponent},
+  {path: 'carrito', component: CarritoComponent},
+  {path: 'perfil', component: PerfilComponent},
+  {path: '**', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

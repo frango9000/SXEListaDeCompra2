80303f1d8cf1803f5cc7007a1bd7c0fa3a76cf07
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2020-03-16 13:34:06 +0100

    responsive header y footer componentes de navegacion home.component.html, catalogo.component.html, carrito.component.html, perfil.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {MDBBootstrapModule} from 'angular-bootstrap-md';
 import {ReactiveFormsModule} from '@angular/forms';
+import {CatalogoComponent} from './catalogo/catalogo.component';
+import {CarritoComponent} from './carrito/carrito.component';
+import {HomeComponent} from './home/home.component';
+import {PerfilComponent} from './perfil/perfil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatalogoComponent,
+    CarritoComponent,
+    HomeComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
